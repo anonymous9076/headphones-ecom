8c93a6f01d803daf10cf5edcd798356b2cb60869
anonymous9076 <vijaytushar915@gmail.com>
2024-02-16 02:57:08 +0530

    first

---
+++ product.js
@@ -0,0 +1,77 @@
+ const products = [
+  {
+    "name": "Adcom Vision 7.1 USB Noise Cancelling Super Gaming Over Ear Stereo Headphone",
+    "desc": "Real 7.1 Surround Sound&tHi-fi 50mm Drivers&tOmnidirectional Microphone&tFantasy Red LED Lights&tAdjustable Volume Control Button",
+    "price": 999,
+    "image": "./images/1.png"
+  },
+  {
+    "name": "Adcom Luminosa - Wireless Bluetooth Over-Ear Stereo Headphone",
+    "desc": "360° stereo sound performance&t40mm bass drivers&tPassive noise cancellation&tWireless operating range up to 20 meters&tIn-built FM radio",
+    "price": 799,
+    "image": "./images/2.jpg"
+  },
+  {
+    "name": "Adcom Shuffle Over-Ear Bluetooth Wireless Headphones",
+    "desc": "360° stereo sound performance&tBluetooth v5.0&t40mm bass drivers&tIn-built HD mic&tLong-lasting battery life",
+    "price": 649,
+    "image": "./images/3.jpg"
+  },
+  {
+    "name": "Adcom Junior Studio Wired On-Ear Stereo Headphones",
+    "desc": "90° Adjustable Leather Pad Cushions&t40mm Moving Coil Drivers&tSurround Stereo Sound&tPassive Noise Cancellation&tUniversal Compatibility",
+    "price": 749,
+    "image": "./images/4.jpg"
+  },
+  {
+    "name": "Gorsun A66 Kids Headphones with Volume Limiter & Mic, Audio Sharing",
+    "desc": "Soft comfy ear pads&tCompact and foldable design&tErgonomic lightweight&tUniversal compatibility&tTangle-free nylon cord",
+    "price": 529,
+    "image": "./images/5.jpg"
+  },
+  {
+    "name": "Feline Cat Ear Premium Headphones - with LED Accent Lights, White",
+    "desc": "Headphones with light-up cat ears&tLED lights can be turned on/off or set to blink&tLight switch behind the right ear&tHigh-quality audio output with super bass&tComfortable padded ear cups&tFoldable design",
+    "price": 699,
+    "image": "./images/6.jpg"
+  },
+  {
+    "name": "Ifrogz Impulse 2 Wireless Premium Headphones with Long Battery Life In Grey",
+    "desc": "Headset offers maximum sound efficiency with added usability&tListen to your favorite music with this over-the-head design headset&tEnjoy the best of both worlds with the Wired/Wireless connectivity option&tMini-phone (3.5mm) host interface for reliable data transfer and enhanced performance&tCircumaural design provides optimum comfort and noise isolation",
+    "price": 899,
+    "image": "./images/7.jpg"
+  },
+  {
+    "name": "Ifrogz Impulse 2 Wireless Premium Headphones with Long Battery Life In Black",
+    "desc": "Headset offers maximum sound efficiency with added usability&tListen to your favorite music with this over-the-head design headset&tEnjoy the best of both worlds with the Wired/Wireless connectivity option&tMini-phone (3.5mm) host interface for reliable data transfer and enhanced performance&tCircumaural design provides optimum comfort and noise isolation",
+    "price": 899,
+    "image": "./images/8.jpg"
+  },
+  {
+    "name": "BeyerDynamic DT 880 Premium Headphones 250 OHM",
+    "desc": "250 ohms impedance for studio use&tSemi-open design (low attenuation of ambient noise)&tTransparent, spacious, and natural sound&tSingle-sided cable&tIdeal for mixing, editing, mastering&tMade in Germany",
+    "price": 1399,
+    "image": "./images/9.jpg"
+  },
+  {
+    "name": "Phiaton BT 460 Wireless Bluetooth Headphones with Touch Interface",
+    "desc": "Music Play Time: 20 hrs&tCall Time: 20 hrs&tStandby Time: 110 hrs&tFull Charge Time: 3 hrs&tOperating Range: Within 10/33 ft",
+    "price": 799,
+    "image": "./images/10.jpg"
+  },
+  {
+    "name": "Contixo KB-2600 Premium Headphones for Kids 85db Volume Limiter - Over-Ear and Build-in Mic Wireless",
+    "desc": "5-in-1 functionality (FM/AM)&tBluetooth&t3.5mm&tMicro SD Card&tBuilt-in microphone for answering phone calls easily&tSoft comfortable material for extended listening&t85db volume limiter&tFlat 3.5mm cable for durable functionality",
+    "price": 599,
+    "image": "./images/11.jpg"
+  },
+  {
+    "name": "Luxmo Trucker Bluetooth Headset, Wireless Headphones with Microphone",
+    "desc": "Chipset: CSR BC8615&tBluetooth Version: V5.0&tSupport function: Answer/Reject phone call, Voice-Dial, VOL+/VOL-. Mute&tListen Music: Next song/Previous song, Play/Pause&tWorking Range: 33 ft&tSize: 163mm x 142mm x 48mm&tHeadset net weight: 1.8 oz./51g&tTalking time: Up to 16 hours&tStandby time: Up to 200 hours&tBattery Capacity: 180mAh / 3.7V",
+    "price": 499,
+    "image": "./images/12.png"
+  }
+]
+
+  
+  export default products
